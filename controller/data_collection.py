7d62b7c9bbbb45af199d2bd1ddd62e5d70603215
Alexander Topolnitskii <taa13102016@gmail.com>
2024-05-09 14:19:30 +0300

    current state

--- controller/data_collection.py
+++ controller/data_collection.py
@@ -1,12 +1,15 @@
-import rospy
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Point, Twist
-from hector_uav_msgs.srv import EnableMotors
-from hector_uav_msgs.msg import Altimeter
-from sensor_msgs.msg import PointCloud2, Image, Range
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import rospy
+import time
+import os
+
+from hector_uav_msgs.srv import EnableMotors
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Twist
+from sensor_msgs.msg import Image, Range
 from cv_bridge import CvBridge, CvBridgeError
 
 Kz = 0.2
@@ -15,24 +18,32 @@ Bz = 0.5
 Ky = 0.05
 By = 0.01
 
+Kx = 0.05
+Bx = 0.01
+
+Kx_1 = 0.03
+Bx_1 = 0.03
+
+Ky_1 = 0.03
+By_1 = 0.03
+
 Kw = 0.1
 Bw = 0.0001
 
 
-des_range = 3.5
-
-
-start_pos = (25, -30)
+des_range = 7
 
 
 class PathNode:
     def __init__(self, rate=10) -> None:
 
         rospy.init_node("controller_node")
+        self.pile_info = PileInfromation()
+
         rospy.Subscriber("/ground_truth/state", Odometry, self.state_callback)  
         #rospy.Subscriber("/asus_camera/rgb/image_raw", Image, self.image_callback_rgb)
         #rospy.Subscriber("/asus_camera/depth/image_raw", Image, self.image_callback_depth)
-        rospy.Subscriber("/downward_cam/downward_camera/image", Image, self.define_borders)
+        rospy.Subscriber("/downward_cam/downward_camera/image", Image, self.follow_border)
         rospy.Subscriber("/sonar_height", Range, self.read_laser_data_alt)
         self.cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1) 
         self.rate = rospy.Rate(rate)
@@ -49,7 +60,9 @@ class PathNode:
 
         self.dot_position = None
         self.line_offset = True
-        self.offset_dist = 1
+        self.offset_dist = 0.5
+
+        self.counter = 0
 
     
     @staticmethod
@@ -81,7 +94,7 @@ class PathNode:
         except CvBridgeError as e:
             print(e)
 
-    def define_borders(self, msg):
+    def follow_border(self, msg):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError as e:
@@ -137,11 +150,22 @@ class PathNode:
         self.twist = msg.twist.twist
 
     def spin(self):
+        pile_height = []
+        coordinates = 'coordinates.txt'
+        height = 'height.txt'
         time_st = rospy.get_time()
         while not rospy.is_shutdown():
             current_time = rospy.get_time()
             elapsed_time = current_time - time_st
-            if elapsed_time < 2:
+            point_index = 0
+            
+            if elapsed_time < 10:
+                uz = Kz * (des_range - self.position.z) - Bz * self.twist.linear.z
+                ux = 0
+                uy = 0
+                uw = 0
+
+            elif elapsed_time >= 10 and elapsed_time  <= 14:
                 uz = Kz * (des_range - self.position.z) - Bz * self.twist.linear.z
                 ux = 0.5
                 uy = 0
@@ -152,12 +176,79 @@ class PathNode:
                     uy = Ky * self.y_error - By * (self.y_error - self.y_error_prev) / (1.0 / 10.0) + self.offset_dist
                 else:
                     uy = Ky * self.y_error - By * (self.y_error - self.y_error_prev) / (1.0 / 10.0)
-                ux = 1
+                ux = 1.0
                 self.y_error_prev = self.y_error
 
-                uz = Kz * (des_range - self.current_range) - Bz * self.twist.linear.z
+                uz = Kz * (des_range - self.position.z) - Bz * self.twist.linear.z
+                # uz = Kz * (des_range - self.current_range) - Bz * self.twist.linear.z
                 uw = Kw * self.omega_error - Bw * (self.omega_error - self.omega_error_prev) / (1.0 / 50.0)
                 self.omega_error_prev = self.omega_error
+                pile_height.append(self.current_range)
+                print(f'Длина pile_height = {len(pile_height)}')
+                self.pile_info.contour_info(coordinates=self.dot_position)
+                self.pile_info.drone_positions(pose_x=self.position.x,
+                                               pose_y=self.position.y,
+                                               pose_z=self.position.z)
+
+                if elapsed_time > 30:
+                    self.pile_info.define_loop(finish_pose=self.position)
+                    if self.pile_info.flag:
+
+                        if not os.path.exists(coordinates):
+                            with open(coordinates, 'w') as file:
+                                for item in self.pile_info.exploration_trajectory:
+                                    file.write(str(item) + '\n')
+                            print(f"Список сохранен в файл {coordinates}")
+
+                        uy = Ky * self.y_error - By * (self.y_error - self.y_error_prev) / (1.0 / 10.0)
+                        ux = 1.0
+                        self.y_error_prev = self.y_error
+
+                        uz = Kz * (des_range - self.position.z) - Bz * self.twist.linear.z
+                        # uz = Kz * (des_range - 1 - self.current_range) - Bz * self.twist.linear.z
+                        uw = Kw * self.omega_error - Bw * (self.omega_error - self.omega_error_prev) / (1.0 / 50.0)
+                        self.omega_error_prev = self.omega_error
+
+                        min_x = min(coord[0] for coord in self.pile_info.exploration_trajectory[:len(self.pile_info.contour_coordinates)])
+                        max_x = max(coord[0] for coord in self.pile_info.exploration_trajectory[:len(self.pile_info.contour_coordinates)])
+                        min_y = min(coord[1] for coord in self.pile_info.exploration_trajectory[:len(self.pile_info.contour_coordinates)]) 
+                        max_y = max(coord[1] for coord in self.pile_info.exploration_trajectory[:len(self.pile_info.contour_coordinates)])
+                        print(f'max_x = {max_x}, min_x = {min_x}')
+                        print(f'max_y = {max_y}, min_y = {min_y}')
+
+
+                        # points = [[-5, -7], 
+                        #           [-5, 3], 
+                        #           [3, 3], 
+                        #           [-5, 3],
+                        #           [0, 0]]
+                        # y_error_prev_p = 0
+                        # x_error_prev_p = 0
+                        # if self.counter < len(points):
+                        #     target_x, target_y = points[self.counter]
+                        #     # Calculate errors based on current position and target point
+                        #     x_error_p = target_x - self.position.x
+                        #     y_error_p = target_y - self.position.y
+
+                        #     # Calculate control inputs based on errors
+                        #     ux = Kx_1 * x_error_p - Bx_1 * self.twist.linear.x
+                        #     uy = Ky_1 * y_error_p - By_1 * self.twist.linear.y
+                        #     print('----------------')
+                        #     uz = Kz * (des_range - self.current_range) - Bz * self.twist.linear.z
+                        #     uw = 0
+                        #     y_error_prev_p = y_error_p
+                        #     x_error_prev_p = x_error_p
+                        #     print(f'Errors: {x_error_p}, {y_error_p}')
+
+                        #     if np.allclose([x_error_p, y_error_p], [0, 0], rtol=2, atol=2):
+                        #         self.counter += 1
+                        # else:
+                        #     ux = 0
+                        #     uy = 0
+                        #     uz = Kz * (des_range - self.current_range) - Bz * self.twist.linear.z
+                        #     uw = 0
+                            
+
 
             cmd_msg = Twist() 
             cmd_msg.linear.z = uz
@@ -165,9 +256,41 @@ class PathNode:
             cmd_msg.linear.y = uy
             cmd_msg.angular.z = -uw
             self.cmd_pub.publish(cmd_msg)
-
             self.rate = rospy.sleep(0.1)
 
+
+
+class PileInfromation():
+    def __init__(self) -> None:
+        print(f'Instance of {self.__class__.__name__} was created!')
+        self.middle_point = []
+        self.flag = False
+        self.contour_coordinates = []
+        self.exploration_start_point = tuple()
+        self.exploration_end_point = list()
+        self.pile_height = []
+        self.exploration_trajectory = []
+
+    def __str__(self) -> str:
+        return f"Storage class for infomation about the pile's coordinates"
+
+    def contour_info(self, coordinates: tuple = None):
+        if not self.flag:
+            self.contour_coordinates.append(coordinates)
+    
+    def drone_positions(self, pose_x: float = None, pose_y: float = None, pose_z: float = None):
+        coords = tuple([pose_x, pose_y, pose_z])
+        self.exploration_trajectory.append(coords)
+
+    # TODO: make start_pose the real start pose, not the first point after time
+    def define_loop(self, start_pose: list = None, finish_pose: list = None):
+        start_pose = np.array([self.exploration_trajectory[0]], dtype=float)
+        finish_pose = np.array([finish_pose.x, finish_pose.y, finish_pose.z], dtype=float)
+        if np.allclose(start_pose, finish_pose, rtol=1, atol=1):
+            self.flag = True
+    
+    def follow_exploration_trajectory(self):
+        pass
 
 def main():
     ctrl = PathNode()
--- controller/read.py
+++ controller/read.py
@@ -5,21 +5,21 @@ from cv_bridge import CvBridge
 import numpy as np
 
 # Specify the path to your ROS bag file
-BAG_FILE = '/home/s/dipl/src/hector_quadrotor/controller/solid_small35.bag'
+BAG_FILE = '/home/s/dipl/src/hector_quadrotor/controller/base.bag'
 
 # Specify the image topics you want to extract
 IMAGE_TOPIC_RGB = '/asus_camera/rgb/image_raw'
 IMAGE_TOPIC_DEPTH = '/asus_camera/depth/image_raw'
 
 # Specify the output directories for the extracted images
-OUTPUT_DIR_RGB = 'controller/solid/small/image'
-OUTPUT_DIR_DEPTH = 'controller/solid/small/depth'
+OUTPUT_DIR_RGB = 'controller/base/'
+#OUTPUT_DIR_DEPTH = 'controller/solid/small/depth'
 
 # Create the output directories if they don't exist
 if not os.path.exists(OUTPUT_DIR_RGB):
     os.makedirs(OUTPUT_DIR_RGB)
-if not os.path.exists(OUTPUT_DIR_DEPTH):
-    os.makedirs(OUTPUT_DIR_DEPTH)
+# if not os.path.exists(OUTPUT_DIR_DEPTH):
+#     os.makedirs(OUTPUT_DIR_DEPTH)
 
 # Initialize the CvBridge class
 bridge = CvBridge()
@@ -31,20 +31,21 @@ with rosbag.Bag(BAG_FILE, 'r') as bag:
         cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
         cv2.imwrite(os.path.join(OUTPUT_DIR_RGB, f"frame{i}.png"), cv_image)
         i += 1
+        print(i)
     
-    # Extract and save Depth images
-    j = 0
-    for topic, msg, t in bag.read_messages(topics=[IMAGE_TOPIC_DEPTH]):
-        try:
-            # Attempt to convert depth images to OpenCV format
-            cv_image = bridge.imgmsg_to_cv2(msg, "passthrough")
-            # Normalize the depth image for visualization
-            cv_image_normalized = cv2.normalize(cv_image, None, 0, 255, cv2.NORM_MINMAX)
-            cv_image_normalized = np.uint8(cv_image_normalized)
-            colored_depth_image = cv2.applyColorMap(cv_image_normalized, cv2.COLORMAP_WINTER)
-            cv2.imwrite(os.path.join(OUTPUT_DIR_DEPTH, f"frame{j}.png"), colored_depth_image)
-            j += 1
-        except Exception as e:
-            print(f"Error processing depth image: {e}")
+    # # Extract and save Depth images
+    # j = 0
+    # for topic, msg, t in bag.read_messages(topics=[IMAGE_TOPIC_DEPTH]):
+    #     try:
+    #         # Attempt to convert depth images to OpenCV format
+    #         cv_image = bridge.imgmsg_to_cv2(msg, "passthrough")
+    #         # Normalize the depth image for visualization
+    #         cv_image_normalized = cv2.normalize(cv_image, None, 0, 255, cv2.NORM_MINMAX)
+    #         cv_image_normalized = np.uint8(cv_image_normalized)
+    #         colored_depth_image = cv2.applyColorMap(cv_image_normalized, cv2.COLORMAP_WINTER)
+    #         cv2.imwrite(os.path.join(OUTPUT_DIR_DEPTH, f"frame{j}.png"), colored_depth_image)
+    #         j += 1
+    #     except Exception as e:
+    #         print(f"Error processing depth image: {e}")
 
 print("Image extraction complete.")
